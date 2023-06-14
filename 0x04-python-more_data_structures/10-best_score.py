8a28c1fbb8007d6e79f2cb40875e1e76950a49c5
gilbert <elormnizi@gmail.com>
2023-06-14 02:39:13 -0700

    correction to task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     maxval = 0
     maxkey = None
     for keypair, val in a_dictionary.items():
-        if ValueError > maxval:
+        if val > maxval:
             maxval = val
             maxkey = keypair
     return maxkey
